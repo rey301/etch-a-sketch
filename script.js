50f08d087de4740948aa4de3159784d125247f8b
rey301 <abrahamrey3001@gmail.com>
2022-11-15 21:26:52 +0000

    Add behaviour on grid to change colour hover

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const grid = document.querySelector('.grid');
 for (let i=0; i < 16; i++) {
     const div = document.createElement('div');
     div.classList.add(`cell-${i}`)
+
+    div.addEventListener('mouseover', e => 
+        e.target.classList.add('hover-over'), {once: true});
+
     grid.appendChild(div);
 }
