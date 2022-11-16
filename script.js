fa2c82837652b54eeb4b339ba4c3ec71ec540f6f
rey301 <abrahamrey3001@gmail.com>
2022-11-16 10:51:07 +0000

    Add edit grid button and change flexbox to grid

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-const grid = document.querySelector('.grid');
+// used to create a grid with numSquares as the number of squares per side
+function createGrid(numSquares) {
+    const grid = document.querySelector('.grid');
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
 
-for (let i=0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add(`cell-${i}`)
+    for (let i=0; i < numSquares**2; i++) {
+        const div = document.createElement('div');
+        div.classList.add(`cell-${i}`);
 
-    div.addEventListener('mouseover', e => 
-        e.target.classList.add('hover-over'), {once: true});
+        div.addEventListener('mouseover', e => 
+            e.target.classList.add('hover-over'), {once: true}
+        );
+        grid.appendChild(div);
+    }
 
-    grid.appendChild(div);
+    grid.style.cssText = `grid-template-columns: repeat(${numSquares}, 1fr); `;
 }
+
+// edit grid button
+const editBtn = document.querySelector('.edit-button');
+editBtn.addEventListener('click', e => {
+    let numSquares = prompt("Enter new number of squares per side", 16);
+
+    if (numSquares > 100 || numSquares < 16) {
+        numSquares = prompt("Number has to be between 16 - 100!", 16);
+    } 
+
+    createGrid(numSquares);
+});
+
+// initial grid
+createGrid(16);
