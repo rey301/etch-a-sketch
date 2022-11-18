922c4d3e7fe111ba8c38f02c7859c438f145cac7
rey301 <abrahamrey3001@gmail.com>
2022-11-18 12:20:14 +0000

    Add randomnised initial colour on rainbow mode

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
+// initialise first pixel that's hovered over
+function randomniseRainbow() {
+    const red = [255, 0, Math.floor(Math.random() * 255)];
+    const pink = [Math.floor(Math.random() * 255), 0, 255];
+    const blue = [0, Math.floor(Math.random() * 255), 255];
+    const green = [0, 255, Math.floor(Math.random() * 255)];
+    const yellow = [Math.floor(Math.random() * 255), 255, 0];
+    const orange = [255, Math.floor(Math.random() * 255), 0];
+    const rgbArray = [red, pink, blue, green, yellow, orange];
+
+    return rgbArray[Math.floor(Math.random()*6)];
+}
+
+// increment rgb values to create rainbow effect
 function incrementRgbVals(rgbVals) {
-    // [255, 0, 0]
-    let incVal = 50;
+    let incVal = 25;
     let r = rgbVals[0];
     let g = rgbVals[1];
     let b = rgbVals[2];
 
-    if (r === 255 && g === 0 && b < 255) {
-        rgbVals[2] += incVal;
-        if (rgbVals[2] > 255) {
-            rgbVals[2] = 255;
-        }
-    } else if (r > 0 && g === 0 && b === 255) {
-        rgbVals[0] -= incVal;
-        if (rgbVals[0] < 0) {
-            rgbVals[0] = 0;
-        }
-    } else if (r === 0 && g < 255 && b === 255) {
-        rgbVals[1] += incVal;
-        if (rgbVals[1] > 255) {
-            rgbVals[1] = 255;
-        }
-    } else if (r === 0 && g === 255 && b > 0) {
-        rgbVals[2] -= incVal
-        if (rgbVals[2] < 0) {
-            rgbVals[2] = 0;
-        }
-    } else if (r < 255 && g === 255 && b === 0) {
-        rgbVals[0] += incVal;
-        if (rgbVals[0] > 255) {
-            rgbVals[0] = 255;
-        }
-    } else if (r === 255 && g > 0 && b === 0) {
-        rgbVals[1] -= incVal;
-        if (rgbVals[1] < 0) {
-            rgbVals[1] = 0;
-        }
-    }
+    if (r === 255 && g < 255 && b === 0) {
+        (g + incVal) > 255 ? rgbVals[1] = 255 : rgbVals[1] += incVal; 
+    } else if (r > 0 && g === 255 && b === 0) {
+        (r - incVal) < 0 ? rgbVals[0] = 0 : rgbVals[0] -= incVal; 
+    } else if (r === 0 && g === 255 && b < 255) {
+        (b + incVal) > 255 ? rgbVals[2] = 255 : rgbVals[2] += incVal; 
+    } else if (r === 0 && g > 0 && b === 255) {
+        (g - incVal) < 0 ? rgbVals[1] = 0 : rgbVals[1] -= incVal; 
+    } else if (r < 255 && g === 0 && b === 255) {
+        (r + incVal) > 255 ? rgbVals[0] = 255 : rgbVals[0] += incVal; 
+    } else if (r === 255 && g === 0 && b > 0) {
+        (b - incVal) < 0 ? rgbVals[2] = 0 : rgbVals[2] -= incVal;
+    } 
 
     return rgbVals;
 }
 
+// calculate 10% of the value used to fade a pixel
 function calcFadeVal(val) {
     return Math.ceil(val/10);
 }
@@ -47,7 +43,7 @@ function calcFadeVal(val) {
 // create a grid with numSquares as the number of squares per side
 function createGrid() {
     const grid = document.querySelector('.grid');
-    let rgbVals = [255, 0, 0];
+    let rgbVals = randomniseRainbow();
     while (grid.firstChild) {
         grid.removeChild(grid.lastChild);
     }
